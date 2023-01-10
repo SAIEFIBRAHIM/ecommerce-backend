efe4b7ec3d86f890455c9d36a4e3fbdb14328928
saiefbrahim <seefbrahim@gmail.com>
2023-01-10 18:11:04 +0100

    fixing all routes and ctrl of addresses

--- controllers/addresses.js
+++ controllers/addresses.js
@@ -3,32 +3,7 @@ const Countries = require("../models/countries");
 const Addresses = require("../models/addresses");
 
 exports.addAddress = async (req, res, next) => {
-  const Country = await Countries.findOne({
-    $or: [
-      { _id: req.body.country },
-      {
-        country: `${req.body.country.charAt(0).toUpperCase()}${req.body.country
-          .slice(1)
-          .toLowerCase()}`,
-      },
-    ],
-  });
-  const State = await States.findOne({
-    $or: [
-      { _id: req.body.state },
-      {
-        state: `${req.body.state.charAt(0).toUpperCase()}${req.body.state
-          .slice(1)
-          .toLowerCase()}`,
-      },
-    ],
-  });
-
-  const Address = new Addresses({
-    ...req.body,
-    country: Country._id,
-    state: State._id,
-  });
+  const Address = new Addresses({ ...req.body });
   Address.save()
     .then((data) => {
       return res.status(201).json({ success: true, data: data });
@@ -40,32 +15,7 @@ exports.addAddress = async (req, res, next) => {
 
 exports.addAddresses = (req, res, next) => {
   req.body.forEach(async (address) => {
-    const Country = await Countries.findOne({
-      $or: [
-        { _id: address.country },
-        {
-          country: `${address.country.charAt(0).toUpperCase()}${address.country
-            .slice(1)
-            .toLowerCase()}`,
-        },
-      ],
-    });
-    const State = await States.findOne({
-      $or: [
-        { _id: address.state },
-        {
-          state: `${address.state.charAt(0).toUpperCase()}${address.state
-            .slice(1)
-            .toLowerCase()}`,
-        },
-      ],
-    });
-
-    const Address = new Addresses({
-      ...address,
-      country: Country._id,
-      state: State._id,
-    });
+    const Address = new Addresses({ ...address });
     Address.save()
       .then((data) => {
         return res.status(201).json({ success: true, data: data });
@@ -97,28 +47,8 @@ exports.getAddresses = (req, res, next) => {
     });
 };
 exports.getAddressesByCountryAndState = async (req, res, next) => {
-  const Country = await Countries.findOne({
-    $or: [
-      { _id: req.query.country },
-      {
-        country: `${req.query.country
-          .charAt(0)
-          .toUpperCase()}${req.query.country.slice(1).toLowerCase()}`,
-      },
-    ],
-  });
-  const State = await States.findOne({
-    $or: [
-      { _id: req.query.state },
-      {
-        state: `${req.query.state.charAt(0).toUpperCase()}${req.query.state
-          .slice(1)
-          .toLowerCase()}`,
-      },
-    ],
-  });
   if (req.query.populate === "info") {
-    Addresses.find({ country: Country._id, state: State._id })
+    Addresses.find({ country: req.query.country, state: req.query.state })
       .populate("Countries", "_id", "country")
       .populate("States", "_id", "state")
       .then((data) => {
@@ -128,7 +58,7 @@ exports.getAddressesByCountryAndState = async (req, res, next) => {
         return res.status(400).json({ success: false, error: error });
       });
   }
-  Addresses.find({ country: Country._id, state: State._id })
+  Addresses.find({ country: req.query.country, state: req.query.state })
     .then((data) => {
       return res.status(200).json({ success: true, data: data });
     })
@@ -138,16 +68,7 @@ exports.getAddressesByCountryAndState = async (req, res, next) => {
 };
 exports.getAddress = (req, res, next) => {
   if (req.query.populate === "info") {
-    Addresses.findOne({
-      $or: [
-        { _id: req.params.address },
-        {
-          state: `${req.params.address
-            .charAt(0)
-            .toUpperCase()}${req.params.address.slice(1).toLowerCase()}`,
-        },
-      ],
-    })
+    Addresses.findById(req.params.id)
       .populate("Countries", "_id", "country")
       .populate("States", "_id", "state")
       .then((data) => {
@@ -157,16 +78,7 @@ exports.getAddress = (req, res, next) => {
         return res.status(400).json({ success: false, error: error });
       });
   }
-  Addresses.findOne({
-    $or: [
-      { _id: req.params.address },
-      {
-        state: `${req.params.address
-          .charAt(0)
-          .toUpperCase()}${req.params.address.slice(1).toLowerCase()}`,
-      },
-    ],
-  })
+  Addresses.findById(req.params.id)
     .then((data) => {
       return res.status(200).json({ success: true, data: data });
     })
@@ -175,63 +87,16 @@ exports.getAddress = (req, res, next) => {
     });
 };
 exports.updateAddress = async (req, res, next) => {
-  const Country = await Countries.findOne({
-    $or: [
-      { _id: req.body.country },
-      {
-        country: `${req.body.country.charAt(0).toUpperCase()}${req.body.country
-          .slice(1)
-          .toLowerCase()}`,
-      },
-    ],
-  });
-  const State = await Countries.findOne({
-    $or: [
-      { _id: req.body.state },
-      {
-        state: `${req.body.state.charAt(0).toUpperCase()}${req.body.state
-          .slice(1)
-          .toLowerCase()}`,
-      },
-    ],
-  });
-  await Addresses.findOne({
-    $or: [
-      { _id: req.params.address },
-      {
-        address: `${req.params.address
-          .charAt(0)
-          .toUpperCase()}${req.params.address.slice(1).toLowerCase()}`,
-      },
-    ],
-  })
-    .then(async (data) => {
-      Addresses.updateOne(
-        { _id: data._id },
-        { ...req.body, country: Country._id, state: State._id }
-      )
-        .then((result) => {
-          return res.status(200).json({ success: true, data: result });
-        })
-        .catch((error) => {
-          return res.status(400).json({ success: false, error: error });
-        });
+  await Addresses.findByIdAndUpdate(req.params.id)
+    .then((data) => {
+      return res.status(200).json({ success: true, data: data });
     })
-    .catch((err) => {
-      return res.status(400).json({ success: false, error: err });
+    .catch((error) => {
+      return res.status(400).json({ success: false, error: error });
     });
 };
 exports.deleteAddress = (req, res, next) => {
-  Addresses.findOneAndDelete({
-    $or: [
-      { _id: req.params.address },
-      {
-        address: `${req.params.address
-          .charAt(0)
-          .toUpperCase()}${req.params.address.slice(1).toLowerCase()}`,
-      },
-    ],
-  })
+  Addresses.findByIdAndDelete(req.params.id)
     .then((data) => {
       return res.status(200).json({ deleted: true, data: data });
     })
--- routes/addresses.js
+++ routes/addresses.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var addressesCtrl = require("../controllers/addresses");
 var auth = require("../middlewares/auth");
 router.get("/", addressesCtrl.getAddresses);
-router.get("/:address", addressesCtrl.getAddress);
+router.get("/:id", addressesCtrl.getAddress);
 router.get("/", addressesCtrl.getAddressesByCountryAndState);
 router.post("/", auth, addressesCtrl.addAddress);
 router.post("/bulk", auth, addressesCtrl.addAddresses);
-router.put("/:address", auth, addressesCtrl.updateAddress);
-router.delete("/:address", auth, addressesCtrl.deleteAddress);
+router.put("/:id", auth, addressesCtrl.updateAddress);
+router.delete("/:id", auth, addressesCtrl.deleteAddress);
 
 module.exports = router;
