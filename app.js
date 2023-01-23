5a9f8f655c00db26c537336786eaf33f05546a88
Saief Brahim <seefbrahim@gmail.com>
2023-01-23 21:32:18 +0100

    finishing signup page last3

--- app.js
+++ app.js
@@ -43,7 +43,11 @@ app.set("view engine", "ejs");
 // CROS access setup
 app.use(
   cors({
-    origin: ["https://seebrand.vercel.app", "http://localhost:4000"],
+    origin: [
+      "https://seebrand.vercel.app",
+      "https://seebrand.vercel.app/signup",
+      "http://localhost:4000",
+    ],
   })
 );
 
